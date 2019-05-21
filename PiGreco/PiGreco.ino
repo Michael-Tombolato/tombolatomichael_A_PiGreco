1dc396908efe261a47182eddc3427c04f05a573b
Michael <michael.tombolato@barsanti.edu.it>
2019-05-21 17:49:37 +0200

    21/05/2019 - Aumento del punteggio

--- PiGreco/PiGreco.ino
+++ PiGreco/PiGreco.ino
@@ -7,8 +7,9 @@ int button3 = 10;
 int button4 = 11;
 int button5 = 12;
 byte cuore[8] = {B00000, B01010, B11111, B11111, B11111, B01110, B00100, B00000};
-int punteggio = 0;
+int punti = 0;
 double tempo = 3000;
+int posizione = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,41 +21,67 @@ void setup() {
   lcd.begin(16, 2);
   lcd.createChar(0, cuore);
   randomSeed(analogRead(0));
+  for (int i = 0; i < 5; i++)
+  {
+    lcd.setCursor(0, 1);
+    lcd.write("b");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  StampaVite();
+  BottoniOff();
+  StampaVitePunti();
   StampaPiGreco();
+  PremiBottone();
+  BottoniOff();
 }
 
-void StampaVite()
+void StampaVitePunti()
 {
   lcd.setCursor(0,0);
   lcd.write(byte(0));
   lcd.write(byte(0));
   lcd.write(byte(0));
+  lcd.setCursor(7, 0);
+  lcd.write("Punti: " + punti);
 }
 
 void StampaPiGreco()
 {
-  int posizione = random(6, 12);
+  posizione = random(6, 12);
   lcd.setCursor(posizione, 1);
   lcd.write("a");
   delay(tempo);
   lcd.clear();
 }
-void Premibottone()
+void PremiBottone()
 {
-  if ()
+  if (posizione == 6 && digitalRead(button1) == HIGH)
   {
-    // se il buttone è on nella stessa posizione del pigreco allora punti++ sennò qualcosa
+    punti++;
+  }
+  else if (posizione == 7 && digitalRead(button2) == HIGH)
+  {
+    punti++;
+  }
+  else if (posizione == 8 && digitalRead(button3) == HIGH)
+  {
+    punti++;
+  }
+  else if (posizione == 9 && digitalRead(button4) == HIGH)
+  {
+    punti++;
+  }
+  else if (posizione == 10 && digitalRead(button5) == HIGH)
+  {
+    punti++;
   }
 }
 
 void AccorciaTempo()
 {
-  if(punteggio % 5 == 0)
+  if(punti % 5 == 0)
   {
     tempo = tempo / 0,25;
   }
@@ -62,5 +89,9 @@ void AccorciaTempo()
 
 void BottoniOff()
 {
-  //usare all'inizio e alla ine per mettere i bottoni off
+  digitalRead(button1) == LOW;
+  digitalRead(button2) == LOW;
+  digitalRead(button3) == LOW;
+  digitalRead(button4) == LOW;
+  digitalRead(button5) == LOW;
 }
